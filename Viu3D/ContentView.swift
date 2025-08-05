cd631f71a7ebba088e2c24518d1cea2961739e72
zach.duan <zach.duan@outlook.com>
2025-08-05 10:25:03 +0800

    Initial Commit

---
+++ Viu3D/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Viu3D
+//
+//  Created by zach duan on 2025/8/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Viu3D/Viu3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Viu3DApp.swift
+//  Viu3D
+//
+//  Created by zach duan on 2025/8/5.
+//
+
+import SwiftUI
+
+@main
+struct Viu3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
